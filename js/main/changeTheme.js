c835df9c08188b9075f1b540988f7593dff61a88
Marcos Willian <markusuuuu@gmail.com>
2019-06-23 10:33:02 -0300

    intervention in mobile bug fixed.

--- js/main/changeTheme.js
+++ js/main/changeTheme.js
@@ -4,6 +4,67 @@
   const listbtn = document.querySelector('[data-change="list-btn"]');
   const events = ['touchstart', 'click'];
 
+
+  const { body } = document;
+
+  const changeTheme = (event) => {
+    const eventTarget = event.target.classList;
+
+    const themeNames = ['t-light-red', 't-light-purple', 't-light-orange', 't-light-green', 't-light-blue', 't-dark-red', 't-dark-purple', 't-dark-orange', 't-dark-green', 't-dark-blue'];
+
+    if (eventTarget.contains('light-purple')) {
+      themeNames.forEach((theme) => {
+        body.classList.remove(theme);
+      });
+      body.classList.add('t-light-purple');
+    } else if (eventTarget.contains('light-blue')) {
+      themeNames.forEach((theme) => {
+        body.classList.remove(theme);
+      });
+      body.classList.add('t-light-blue');
+    } else if (eventTarget.contains('light-green')) {
+      themeNames.forEach((theme) => {
+        body.classList.remove(theme);
+      });
+      body.classList.add('t-light-green');
+    } else if (eventTarget.contains('light-orange')) {
+      themeNames.forEach((theme) => {
+        body.classList.remove(theme);
+      });
+      body.classList.add('t-light-orange');
+    } else if (eventTarget.contains('light-red')) {
+      themeNames.forEach((theme) => {
+        body.classList.remove(theme);
+      });
+      body.classList.add('t-light-red');
+    } else if (eventTarget.contains('dark-purple')) {
+      themeNames.forEach((theme) => {
+        body.classList.remove(theme);
+      });
+      body.classList.add('t-dark-purple');
+    } else if (eventTarget.contains('dark-blue')) {
+      themeNames.forEach((theme) => {
+        body.classList.remove(theme);
+      });
+      body.classList.add('t-dark-blue');
+    } else if (eventTarget.contains('dark-green')) {
+      themeNames.forEach((theme) => {
+        body.classList.remove(theme);
+      });
+      body.classList.add('t-dark-green');
+    } else if (eventTarget.contains('dark-orange')) {
+      themeNames.forEach((theme) => {
+        body.classList.remove(theme);
+      });
+      body.classList.add('t-dark-orange');
+    } else if (eventTarget.contains('dark-red')) {
+      themeNames.forEach((theme) => {
+        body.classList.remove(theme);
+      });
+      body.classList.add('t-dark-red');
+    }
+  };
+
   const handleChangeThemeEvent = (event) => {
     event.preventDefault();
     showButton.classList.add('is-show');
@@ -16,81 +77,12 @@
 
     events.forEach((userEvent) => {
       buttons.forEach((btn) => {
-        btn.addEventListener(userEvent, (event) => {
-          event.preventDefault();
-
-          const body = document.body;
-
-          const eventTarget = event.target.classList;
-          const themeNames = ['t-light-red', 't-light-purple', 't-light-orange', 't-light-green', 't-light-blue', 't-dark-red', 't-dark-purple', 't-dark-orange', 't-dark-green', 't-dark-blue'];
-
-          if (eventTarget.contains('light-purple')) {
-            themeNames.forEach((theme) => {
-              body.classList.remove(theme);
-            })
-            body.classList.add('t-light-purple');
-          }
-          else if (eventTarget.contains('light-blue')) {
-            themeNames.forEach((theme) => {
-              body.classList.remove(theme);
-            })
-            body.classList.add('t-light-blue');
-          }
-          else if (eventTarget.contains('light-green')) {
-            themeNames.forEach((theme) => {
-              body.classList.remove(theme);
-            })
-            body.classList.add('t-light-green');
-          }
-          else if (eventTarget.contains('light-orange')) {
-            themeNames.forEach((theme) => {
-              body.classList.remove(theme);
-            })
-            body.classList.add('t-light-orange');
-          }
-          else if (eventTarget.contains('light-red')) {
-            themeNames.forEach((theme) => {
-              body.classList.remove(theme);
-            })
-            body.classList.add('t-light-red');
-          }
-          else if (eventTarget.contains('dark-purple')) {
-            themeNames.forEach((theme) => {
-              body.classList.remove(theme);
-            })
-            body.classList.add('t-dark-purple');
-          }
-          else if (eventTarget.contains('dark-blue')) {
-            themeNames.forEach((theme) => {
-              body.classList.remove(theme);
-            })
-            body.classList.add('t-dark-blue');
-          }
-          else if (eventTarget.contains('dark-green')) {
-            themeNames.forEach((theme) => {
-              body.classList.remove(theme);
-            })
-            body.classList.add('t-dark-green');
-          }
-          else if (eventTarget.contains('dark-orange')) {
-            themeNames.forEach((theme) => {
-              body.classList.remove(theme);
-            })
-            body.classList.add('t-dark-orange');
-          }
-          else if (eventTarget.contains('dark-red')) {
-            themeNames.forEach((theme) => {
-              body.classList.remove(theme);
-            })
-            body.classList.add('t-dark-red');
-          }
-        });
+        btn.addEventListener(userEvent, changeTheme);
       });
     });
-  }
+  };
 
   events.forEach((userEvent) => {
     showButton.addEventListener(userEvent, handleChangeThemeEvent);
   });
-
 }
