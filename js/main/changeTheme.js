e456dd1351412a5891c5a31bbca1a06d3c5a0b50
Marcos Willian <markusuuuu@gmail.com>
2019-06-24 09:34:42 -0300

    melhorado código javascript de mudar os temas e adicionado estado active nos botões.

--- js/main/changeTheme.js
+++ js/main/changeTheme.js
@@ -10,58 +10,26 @@
   const changeTheme = (event) => {
     const eventTarget = event.target.classList;
 
+    // Todos os nomes das classes de temas
     const themeNames = ['t-light-red', 't-light-purple', 't-light-orange', 't-light-green', 't-light-blue', 't-dark-red', 't-dark-purple', 't-dark-orange', 't-dark-green', 't-dark-blue'];
 
-    if (eventTarget.contains('light-purple')) {
+    // Todos os nomes das classes dos botões
+    const btnClass = ['light-purple', 'light-blue', 'light-green', 'light-orange', 'light-red', 'dark-purple', 'dark-blue', 'dark-green', 'dark-orange', 'dark-red'];
+
+    // Se btnClass contiver o mesmo nome que existir na classe do EventTarget
+    // Remove todos os nomes que estão na classe do body
+    // e adciona a classe referente ao Target
+    // depois para cada botão, remove a classe 'ativo'
+    // e adiciona ela apenas para o Target.
+    if (btnClass.includes(eventTarget.value)) {
       themeNames.forEach((theme) => {
         body.classList.remove(theme);
       });
-      body.classList.add('t-light-purple');
-    } else if (eventTarget.contains('light-blue')) {
-      themeNames.forEach((theme) => {
-        body.classList.remove(theme);
+      body.classList.add(`t-${eventTarget.value}`);
+      buttons.forEach((btn) => {
+        btn.classList.remove('active');
       });
-      body.classList.add('t-light-blue');
-    } else if (eventTarget.contains('light-green')) {
-      themeNames.forEach((theme) => {
-        body.classList.remove(theme);
-      });
-      body.classList.add('t-light-green');
-    } else if (eventTarget.contains('light-orange')) {
-      themeNames.forEach((theme) => {
-        body.classList.remove(theme);
-      });
-      body.classList.add('t-light-orange');
-    } else if (eventTarget.contains('light-red')) {
-      themeNames.forEach((theme) => {
-        body.classList.remove(theme);
-      });
-      body.classList.add('t-light-red');
-    } else if (eventTarget.contains('dark-purple')) {
-      themeNames.forEach((theme) => {
-        body.classList.remove(theme);
-      });
-      body.classList.add('t-dark-purple');
-    } else if (eventTarget.contains('dark-blue')) {
-      themeNames.forEach((theme) => {
-        body.classList.remove(theme);
-      });
-      body.classList.add('t-dark-blue');
-    } else if (eventTarget.contains('dark-green')) {
-      themeNames.forEach((theme) => {
-        body.classList.remove(theme);
-      });
-      body.classList.add('t-dark-green');
-    } else if (eventTarget.contains('dark-orange')) {
-      themeNames.forEach((theme) => {
-        body.classList.remove(theme);
-      });
-      body.classList.add('t-dark-orange');
-    } else if (eventTarget.contains('dark-red')) {
-      themeNames.forEach((theme) => {
-        body.classList.remove(theme);
-      });
-      body.classList.add('t-dark-red');
+      eventTarget.add('active');
     }
   };
 
@@ -70,11 +38,13 @@
     showButton.classList.add('is-show');
     listbtn.classList.add('is-show');
 
+    //  Ao clicar fora do container ul, remove a classe
     outsideEvent(listbtn, events, () => {
       showButton.classList.remove('is-show');
       listbtn.classList.remove('is-show');
     });
 
+    // Para cada botão, ativa a função changeTheme
     events.forEach((userEvent) => {
       buttons.forEach((btn) => {
         btn.addEventListener(userEvent, changeTheme);
@@ -82,6 +52,7 @@
     });
   };
 
+  // Evento no primeiro botão, de mostrar o container
   events.forEach((userEvent) => {
     showButton.addEventListener(userEvent, handleChangeThemeEvent);
   });
