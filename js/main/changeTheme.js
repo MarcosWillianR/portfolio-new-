96cbf9d78e2aae0386a2724b2c87623893f8f1c1
Marcos Willian <markusuuuu@gmail.com>
2019-06-30 21:08:30 -0300

    refatoração dos arquivos css e das imagens.

--- js/main/changeTheme.js
+++ js/main/changeTheme.js
@@ -10,22 +10,21 @@
   const changeTheme = (event) => {
     const eventTarget = event.target.classList;
 
-    // Todos os nomes das classes de temas
-    const themeNames = ['t-light-red', 't-light-purple', 't-light-orange', 't-light-green', 't-light-blue', 't-dark-red', 't-dark-purple', 't-dark-orange', 't-dark-green', 't-dark-blue'];
+    const themeColors = ['red', 'blue', 'purple', 'orange', 'green'];
+    const themeBackground = ['t-light', 't-dark'];
 
-    // Todos os nomes das classes dos botões
-    const btnClass = ['light-purple', 'light-blue', 'light-green', 'light-orange', 'light-red', 'dark-purple', 'dark-blue', 'dark-green', 'dark-orange', 'dark-red'];
+    const eventArray = Array.from(eventTarget);
+    const eventThemeBG = eventArray[0];
+    const eventthemeColor = eventArray[1];
 
-    // Se btnClass contiver o mesmo nome que existir na classe do EventTarget
-    // Remove todos os nomes que estão na classe do body
-    // e adciona a classe referente ao Target
-    // depois para cada botão, remove a classe 'ativo'
-    // e adiciona ela apenas para o Target.
-    if (btnClass.includes(eventTarget.value)) {
-      themeNames.forEach((theme) => {
-        body.classList.remove(theme);
+    if (themeColors.includes(eventthemeColor) && themeBackground.includes(eventThemeBG)) {
+      themeColors.forEach((themeColor) => {
+        body.classList.remove(themeColor);
       });
-      body.classList.add(`t-${eventTarget.value}`);
+      themeBackground.forEach((themeBG) => {
+        body.classList.remove(themeBG);
+      });
+      body.classList.add(eventThemeBG, eventthemeColor);
       buttons.forEach((btn) => {
         btn.classList.remove('active');
       });
@@ -38,13 +37,11 @@
     showButton.classList.add('is-show');
     listbtn.classList.add('is-show');
 
-    //  Ao clicar fora do container ul, remove a classe
     outsideEvent(listbtn, events, () => {
       showButton.classList.remove('is-show');
       listbtn.classList.remove('is-show');
     });
 
-    // Para cada botão, ativa a função changeTheme
     events.forEach((userEvent) => {
       buttons.forEach((btn) => {
         btn.addEventListener(userEvent, changeTheme);
@@ -52,7 +49,6 @@
     });
   };
 
-  // Evento no primeiro botão, de mostrar o container
   events.forEach((userEvent) => {
     showButton.addEventListener(userEvent, handleChangeThemeEvent);
   });
