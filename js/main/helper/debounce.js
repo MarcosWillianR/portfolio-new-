69c775999fb2fb9d60eb1f7ca27e49e153478f15
Marcos Willian <markusuuuu@gmail.com>
2019-08-04 10:13:26 -0300

    removido o primeiro projeto

--- js/main/helper/debounce.js
+++
@@ -1,9 +0,0 @@
-function debounce(callback, delay) {
-  let timer;
-  return (...args) => {
-    if (timer) clearTimeout(timer);
-    timer = setTimeout(() => {
-      callback(...args);
-    }, delay);
-  };
-}
--- js/main/view3.js
+++
@@ -1,4 +0,0 @@
-{
-    // Largura minima de large device (1440px) > 215px
-    // altura minima de large device (1440px) >
-}
